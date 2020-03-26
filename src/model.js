d4cf3af112d358b65bc473b4af587210134a364d
Boris Povolotsky <boristofu@gmail.com>
2020-03-26 17:24:00 +0200

    fix index creation mongoose

--- src/model.js
+++ src/model.js
@@ -41,7 +41,7 @@ const TodaySchema = new Schema({
 })
 
 const TodayModel = mongoose.model('today', TodaySchema);
-TodayModel.index({ "createdAt": 1 }, { expireAfterSeconds: 3600 })
+TodaySchema.index({ "createdAt": 1 }, { expireAfterSeconds: 3600 })
 
 const CountryModel = mongoose.model('country', CountrySchema)
 const ResourceModel = mongoose.model('resource', ResourceSchema)
